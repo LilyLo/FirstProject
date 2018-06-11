c473c83b1cbd9516cdf16db5e1ac8fb424add3c6
LilyL <lily.lobko5@gmail.com>
2018-06-12 00:15:32 +0200

    Images v5.0

--- CVtask/Images_script.js
+++ CVtask/Images_script.js
@@ -2,6 +2,14 @@ var img = document.getElementsByClassName('column');
 
 var open = document.getElementById('popup');
 
-img.onclick = function() {
-    open.style.display = "block";
+var close = document.getElementsByClassName("close")[0];
+
+for (var i = 0; i < img.length; i++) {
+    img[i].onclick = function() {
+        open.style.display = "block";
+    }
+}
+
+close.onclick = function() {
+	open.style.display = "none";
 }
