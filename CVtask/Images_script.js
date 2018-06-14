25a1ba863541d17718a33e3c832549e09f6421b0
LilyLo <35495920+LilyLo@users.noreply.github.com>
2018-06-14 16:52:23 +0200

    Images 6.0
    New working version

--- CVtask/Images_script.js
+++ CVtask/Images_script.js
@@ -1,15 +1,28 @@
-var img = document.getElementsByClassName('column');
+var imgCont = document.getElementsByClassName('column');
+
+var popupImage = document.getElementById("popup-image");
 
 var open = document.getElementById('popup');
 
 var close = document.getElementsByClassName("close")[0];
 
-for (var i = 0; i < img.length; i++) {
-    img[i].onclick = function() {
-        open.style.display = "block";
+function openPopup() {
+	open.style.display = "block";
+}
+
+function hidePopup() {
+	open.style.display = "none";
+}
+
+for (var i = 0; i < imgCont.length; i++) {
+	imgCont[i].onclick = function() {
+     	var imgsrc = this.getElementsByTagName("img")[0].getAttribute("src");
+     	popupImage.src = imgsrc;
+     	openPopup();   
     }
 }
 
 close.onclick = function() {
-	open.style.display = "none";
+	hidePopup();
 }
+
