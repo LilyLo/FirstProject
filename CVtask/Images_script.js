aed83ae2aac75d0c018256b70c7f6198c8c2f05c
LilyLo <35495920+LilyLo@users.noreply.github.com>
2018-06-18 18:55:15 +0200

    Images v.5.3

--- CVtask/Images_script.js
+++ CVtask/Images_script.js
@@ -8,7 +8,9 @@ var rightArrow = document.querySelector('#right-arrow');
 
 var leftArrow = document.querySelector('#left-arrow');
 
-var close = document.querySelectorAll(".close")[0];
+var close = document.querySelector(".close");
+
+var originId;
 
 function openPopup() {
 	popup.classList.replace('closepopup', 'openpopup');
@@ -19,21 +21,26 @@ function hidePopup() {
 }
 
 for (var i = 0; i < imageContainer.length; i++) {
+
 	imageContainer[i].onclick = function() {
-     	var imgsrc = this.querySelectorAll("img")[0].getAttribute("src");
+     	var imgsrc = this.querySelector("img").getAttribute("src");
+     	
+     	originId = this.querySelector("img").id;
      	popupImage.src = imgsrc;
      	openPopup();   
-    }
-
-    rightArrow.onclick = function () {
-
-    	var currentImageContainer = popup.querySelector("img").parentElement;
-
-    	var containerNumber =  imageContainer.indexOf(currentImageContainer);
-	}
+    }  
 }
 
+rightArrow.onclick = function() {
+	var currentContainer = document.getElementById(originId).parentElement;
 
+	currentContainer.id = "currentContainer";
+
+	var containerNumber = imageContainer.map(function(x) {return x.id; }).indexOf("currentContainer");
+
+       	popupImage.src = imgsrc;
+     	openPopup();
+}			
 
 close.onclick = function() {
 	hidePopup();
